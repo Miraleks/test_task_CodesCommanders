c48707a37f13c48829415fc92f597c73730bbb0a
Alex Mironositskiy <drive_forever@mail.ru>
2025-05-05 22:57:31 +0200

    refactor: replace user dropdown with ID input in OrderSerializer with validation

--- user_order_project/api/serializers.py
+++ user_order_project/api/serializers.py
@@ -7,11 +7,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Order
         fields = '__all__'
 
     def validate_user(self, value):
-        if not User.objects.filter(id=value.id).exists():
-            raise serializers.ValidationError("User with this ID does not exist.")
+        if not User.objects.filter(id=value).exists():
+            raise serializers.ValidationError("A user with such ID does not exist.")
         return value
+
+    def create(self, validated_data):
+        user_id = validated_data.pop('user')
+        user = User.objects.get(id=user_id)
+        return Order.objects.create(user=user, **validated_data)
+
+    def update(self, instance, validated_data):
+        if 'user' in validated_data:
+            user_id = validated_data.pop('user')
+            if not User.objects.filter(id=user_id).exists():
+                raise serializers.ValidationError("Пользователь с таким ID не существует.")
+            instance.user = User.objects.get(id=user_id)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
