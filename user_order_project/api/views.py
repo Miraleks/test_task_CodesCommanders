979af6069f3fecf71273aa3d5b069c9efb404928
Alex Mironositskiy <drive_forever@mail.ru>
2025-05-05 21:20:44 +0200

    add views

--- user_order_project/api/views.py
+++ user_order_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Order
+from .serializers import UserSerializer, OrderSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
