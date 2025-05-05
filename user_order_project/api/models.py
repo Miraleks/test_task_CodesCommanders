b4e3bf8b7dae104acecd8e378d49d34b160a1e27
Alex Mironositskiy <drive_forever@mail.ru>
2025-05-05 21:17:31 +0200

    Create models

--- user_order_project/api/models.py
+++ user_order_project/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return self.title
