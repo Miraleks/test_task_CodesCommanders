685b8089d7b12f3403c7a806ae6d8e6abc6056ea
Alex Mironositskiy <drive_forever@mail.ru>
2025-05-05 21:21:07 +0200

    remove SECRET_KEY inside .env

--- user_order_project/user_order_project/settings.py
+++ user_order_project/user_order_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vk_luo!*y_*$5o6r4cq_4#xoiiqx0-i&zwzc+%pja6uo@5llc-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
